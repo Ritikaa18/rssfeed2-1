1d6794f30ff642fa0a9fec9b2295c34e3c76010d
bodysoda69 <72090512+bodysoda69@users.noreply.github.com>
2021-06-01 08:20:08 +0530

    Update rss.py

--- rss.py
+++ rss.py
@@ -34,13 +34,13 @@ app = Client(":memory:", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
 def check_feed():
     FEED = feedparser.parse(feed_url)
-    entry = FEED.entries[0].link
-    if entry.link != db.get_link(feed_url).link:
+    entry = FEED.entries[0]
+    if entry.id != db.get_link(feed_url).link:
                    # ↓ Edit this message as your needs.
       message = f"/mirror {entry.link}"
       try:
         app.send_message(log_channel, message)
-        db.update_link(feed_url, entry.link)
+        db.update_link(feed_url, entry.id)
       except FloodWait as e:
         print(f"FloodWait: {e.x} seconds")
         sleep(e.x)
@@ -56,13 +56,13 @@ app = Client(":memory:", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
       
 def check_feed1():
     FEED = feedparser.parse(feed_url1)
-    entry = FEED.entries[0].link
-    if entry.link != db.get_link(feed_url1).link:
+    entry = FEED.entries[0]
+    if entry.id != db.get_link(feed_url1).link:
                    # ↓ Edit this message as your needs.
       message = f"/dank {entry.link}"
       try:
         app.send_message(log_channel, message)
-        db.update_link(feed_url1, entry.link)
+        db.update_link(feed_url1, entry.id)
       except FloodWait as e:
         print(f"FloodWait: {e.x} seconds")
         sleep(e.x)
