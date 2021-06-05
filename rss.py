77874f67d79cc36789f36b7112b83ff2e5401a91
bodysoda69 <72090512+bodysoda69@users.noreply.github.com>
2021-06-05 20:30:14 +0530

    Update rss.py

--- rss.py
+++ rss.py
@@ -12,6 +12,8 @@ api_hash = ""   # Get it from my.telegram.org
 feed_url = ""   # RSS Feed URL of the site.
 feed_url1 = ""   # RSS Feed URL of the site.
 feed_url2 = ""   # RSS Feed URL of the site.
+feed_url3 = ""   # RSS Feed URL of the site.
+feed_url4 = ""   # RSS Feed URL of the site.
 bot_token = ""   # Get it by creating a bot on https://t.me/botfather
 log_channel = ""   # Telegram Channel ID where the bot is added and have write permission. You can use group ID too.
 check_interval = 5   # Check Interval in seconds.  
@@ -21,7 +23,9 @@ if os.environ.get("ENV"):   # Add a ENV in Environment Variables if you wanna co
   api_hash = os.environ.get("API_HASH")
   feed_url = os.environ.get("FEED_URL")
   feed_url1 = os.environ.get("FEED_URL1")
-  feed_url2 = os.environ.get("FEED_URL2") 
+  feed_url2 = os.environ.get("FEED_URL2")
+  feed_url3 = os.environ.get("FEED_URL2")
+  feed_url4 = os.environ.get("FEED_URL2") 
   bot_token = os.environ.get("BOT_TOKEN")
   log_channel = int(os.environ.get("LOG_CHANNEL", None))
   check_interval = int(os.environ.get("INTERVAL", 5))
@@ -103,10 +107,64 @@ def check_feed2():
     else:
       print(f"Checked RSS FEED2 - LHD Encodes")
   
+
+if db.get_link(feed_url3) == None:
+   db.update_link(feed_url3, "*")
+
+app = Client(":memory:", api_id=api_id, api_hash=api_hash, bot_token=bot_token)      
       
+def check_feed3():
+    FEED = feedparser.parse(feed_url3)
+    entry = FEED.entries[0]
+    if entry.id != db.get_link(feed_url3).link:
+        if '720p' in entry.title or 'hdtv' in entry.title.lower() or 'yts' in entry.title.lower() or 'avi' in entry.title.lower() or 'xvid' in entry.title.lower() or 'mp4' in entry.title.lower() or 'galaxyrg' in entry.title.lower():
+            message = f"{entry.link}"
+        else:
+                   # ↓ Edit this message as your needs.
+            message = f"/mirror {entry.enclosures[0]['href']}"
+        try:
+          app.send_message(log_channel, message)
+          db.update_link(feed_url3, entry.id)
+        except FloodWait as e:
+          print(f"FloodWait: {e.x} seconds")
+          sleep(e.x)
+        except Exception as e:
+          print(e)
+    else:
+      print(f"Checked RSS FEED3 - TorrentFunk Movies")
+            
+            
+if db.get_link(feed_url4) == None:
+   db.update_link(feed_url4, "*")
+
+app = Client(":memory:", api_id=api_id, api_hash=api_hash, bot_token=bot_token)      
+      
+def check_feed4():
+    FEED = feedparser.parse(feed_url4)
+    entry = FEED.entries[0]
+    if entry.link != db.get_link(feed_url2).link:
+        if '720p' in entry.title or 'hdtv' in entry.title.lower() or 'avi' in entry.title.lower() or 'xvid' in entry.title.lower() or 'mp4' in entry.title.lower():
+            message = f"{entry.link}"
+        else:
+                   # ↓ Edit this message as your needs.
+            message = f"/dank {entry.enclosures[0]['href']}"
+        try:
+          app.send_message(log_channel, message)
+          db.update_link(feed_url4, entry.link)
+        except FloodWait as e:
+          print(f"FloodWait: {e.x} seconds")
+          sleep(e.x)
+        except Exception as e:
+          print(e)
+    else:
+      print(f"Checked RSS FEED4 - TorrentFunk TV")
+            
+
 scheduler = BackgroundScheduler()
 scheduler.add_job(check_feed, "interval", seconds=check_interval, max_instances=max_instances, misfire_grace_time=None)
 scheduler.add_job(check_feed1, "interval", seconds=check_interval, max_instances=max_instances, misfire_grace_time=None)
 scheduler.add_job(check_feed2, "interval", seconds=check_interval, max_instances=max_instances, misfire_grace_time=None)
+scheduler.add_job(check_feed3, "interval", seconds=check_interval, max_instances=max_instances, misfire_grace_time=None)
+scheduler.add_job(check_feed4, "interval", seconds=check_interval, max_instances=max_instances, misfire_grace_time=None)
 scheduler.start()
 app.run()
